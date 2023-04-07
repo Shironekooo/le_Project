2ebc90e6076ca5504ed94a8e883aafaab3467401
yoshi <yoshiomgennai@su.edu.ph>
2023-04-07 20:59:04 +0800

    pushed with dependency issues(2)

---
+++ app/src/main/java/com/example/le_androidapp/data/AccelResult.kt
@@ -0,0 +1,8 @@
+package com.example.le_androidapp.data;
+
+data class AccelResult(
+    val xAccel: Float,
+    val yAccel: Float,
+    val zAccel: Float,
+    val connectionState: ConnectionState
+)
