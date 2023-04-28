b43182ab60df3a58b8c7dcbc90b4d769c97c015c
kegds <101492016+kegds@users.noreply.github.com>
2023-04-29 00:57:08 +0800

    trial register activity

--- app/src/main/java/com/example/le_androidapp/RegisterActivity.java
+++ app/src/main/java/com/example/le_androidapp/RegisterActivity.java
@@ -4,23 +4,35 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView uploadImage;
+    String imageURL;
     Button addbtn;
-    private RadioGroup radioGroupGender;
     EditText firstName, middleName, lastName, userAge, contactNumber, userEmail, userPass;
     Uri uri;
 
@@ -30,6 +42,13 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firstName = findViewById(R.id.userFirst);
+        middleName = findViewById(R.id.userMiddle);
+        lastName = findViewById(R.id.userLast);
+        userAge = findViewById(R.id.userAge);
+        contactNumber = findViewById(R.id.contactNumber);
+        userEmail = findViewById(R.id.userEmail);
+        userPass = findViewById(R.id.userPass);
 
         ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -39,14 +58,92 @@ public class RegisterActivity extends AppCompatActivity {
                         if (result.getResultCode() == Activity.RESULT_OK){
                             Intent data = result.getData();
                             uri = data.getData();
+                            uploadImage.setImageURI(uri);
 
-
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
         );
 
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+            }
+        });
 
 
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+                }
+
+
+        });
+
+    }
+
+    public void saveData(){
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Android Images")
+                .child(uri.getLastPathSegment());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete());
+                Uri urlImage = uriTask.getResult();
+                imageURL = urlImage.toString();
+                uploadData();
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    public void uploadData(){
+
+        String firstN = firstName.getText().toString();
+        String middleN = middleName.getText().toString();
+        String lastN = lastName.getText().toString();
+        String age = userAge.getText().toString();
+        String contactNo = contactNumber.getText().toString();
+        String email = userEmail.getText().toString();
+        String pass = userPass.getText().toString();
+
+        UserClass userClass = new UserClass(firstN, middleN, lastN, age,contactNo,email,pass,imageURL);
+
+        FirebaseDatabase.getInstance().getReference("User Data").child(lastN)
+                .setValue(userClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/le_androidapp/UserClass.java
+++ app/src/main/java/com/example/le_androidapp/UserClass.java
@@ -2,19 +2,20 @@ package com.example.le_androidapp;
 
 public class UserClass {
 
-    private Integer userId;
     private String firstName;
     private String middleName;
     private String lastName;
-    private Integer userAge;
-    private String userGender;
+    private String userAge;
+    //private String userGender;
     private String contactNumber;
     private String userEmail;
     private String userPass;
 
-    public Integer getUserId() {
+    private String dataImage;
+
+    /*public Integer getUserId() {
         return userId;
-    }
+    }*/
 
     public String getFirstName() {
         return firstName;
@@ -28,13 +29,13 @@ public class UserClass {
         return lastName;
     }
 
-    public Integer getUserAge() {
+    public String getUserAge() {
         return userAge;
     }
 
-    public String getUserGender() {
+    /*public String getUserGender() {
         return userGender;
-    }
+    }*/
 
     public String getContactNumber() {
         return contactNumber;
@@ -48,15 +49,20 @@ public class UserClass {
         return userPass;
     }
 
-    public UserClass(Integer userId, String firstName, String middleName, String lastName, Integer userAge, String userGender, String contactNumber, String userEmail, String userPass) {
-        this.userId = userId;
+    public String getDataImage() {
+        return dataImage;
+    }
+
+    public UserClass(String firstName, String middleName, String lastName, String userAge, String contactNumber, String userEmail, String userPass, String dataImage) {
+       // this.userId = userId;
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
         this.userAge = userAge;
-        this.userGender = userGender;
+       // this.userGender = userGender;
         this.contactNumber = contactNumber;
         this.userEmail = userEmail;
         this.userPass = userPass;
+        this.dataImage = dataImage;
     }
 }
